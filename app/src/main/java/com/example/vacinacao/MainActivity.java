f229983f752f48d38364a7f73de1a5c7e082f4f9
VINICIUS DANIEL NEUENFELDT BARROS <viniciusdnbarros@gmail.com>
2020-04-07 00:03:56 -0300

    Commit

---
+++ app/src/main/java/com/example/vacinacao/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.vacinacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //alo
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
