9100cfb3cb9870e07fd098b08743d59501a961e0
leandrothomas478 <leandro-thomas@hotmail.com>
2020-04-20 23:09:32 -0300

    login

---
+++ app/src/main/java/com/example/vacinacao/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.vacinacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /*
+     * Evento do botão login
+     */
+    public void login(View v){
+        //Chamar a outra tela
+    }
+    /*
+     * Evento do botão cadastrar
+     */
+    public void cadastrar(View v){
+        Intent intent = new Intent(this, CadastroPessoaActivity.class);
+        //Chama a tela de cadastro
+        startActivity(intent);
+    }
+}
