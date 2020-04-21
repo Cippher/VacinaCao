b3fa266b983f16274cacc87aeeff6b75da71a46e
VINICIUS DANIEL NEUENFELDT BARROS <viniciusdnbarros@gmail.com>
2020-04-20 23:20:42 -0300

    Primeiro commit da tela de cadastro

---
+++ app/src/main/java/com/example/vacinacao/CadastroPessoaActivity.java
@@ -0,0 +1,16 @@
+package com.example.vacinacao;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CadastroPessoaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_pessoa);
+    }
+
+}
--- app/src/main/java/com/example/vacinacao/MainActivity.java
+++ app/src/main/java/com/example/vacinacao/MainActivity.java
@@ -2,14 +2,38 @@ package com.example.vacinacao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    //alo
+    //Variable declaration
+    EditText txtEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+
+    }
+
+    /*
+    * Evento do botão login
+    */
+    public void login(View v){
+        //Chamar a outra tela
+    }
+    /*
+    * Evento do botão cadastrar
+    */
+    public void cadastrar(View v){
+        Intent intent = new Intent(this, CadastroPessoaActivity.class);
+        //Chama a tela de cadastro
+        startActivity(intent);
     }
 }
