503c53f4f560d820fbf112df3ec5e218214bd1e7
VINICIUS DANIEL NEUENFELDT BARROS <viniciusdnbarros@gmail.com>
2020-05-20 22:37:29 -0300

    Atualização da classe CadastroCaoActivity

--- app/src/main/java/com/example/vacinacao/CadastroCaoActivity.java
+++ app/src/main/java/com/example/vacinacao/CadastroCaoActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,15 +14,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class CadastroCaoActivity  extends Activity implements AdapterView.OnItemSelectedListener{
 
+    EditText txtNome;
+    EditText txtRaca;
+    EditText txtDataNascimento;
+    EditText txtPeso;
+
+    private static final String[] strSexo = new String[]{"M", "F"};
+
+    CadastroCaoDB ccdb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_pet);
-        //coment new for commit
 
+        txtNome =           (EditText) findViewById(R.id.txtNome);
+        txtRaca =           (EditText) findViewById(R.id.txtRaca);
+        txtDataNascimento = (EditText) findViewById(R.id.txtDataNascimento);
+        txtPeso =           (EditText) findViewById(R.id.txtPeso);
+        /*
+         * Spinner
+         */
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(CadastroCaoActivity.this,
+                android.R.layout.simple_spinner_item,strSexo);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnSexo.setAdapter(adapter);
+        spnSexo.setOnItemSelectedListener(this);
+
+        ccdb = new CadastroCaoDB(getBaseContext());
     }
 
     public void confirmar(View v){
+        CadastroCao cc = new CadastroCao();
+
+        cc.setNome(txtNome.getText().toString());
+        cc.setRaca(txtRaca.getText().toString());
+        cc.setDataNascimento();
+        cc.setPeso(Integer.parseInt(txtPeso.getText().toString()));
+        cc.setSexo();
+
+        ccdb.adicionarCadastroCao(cc);
+
         Intent intent = new Intent(this, MenuUsuarioPetActivity.class);
         startActivity(intent);
     }
